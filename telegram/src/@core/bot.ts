bd6d5a9c4cd3332ba8b92088d284f2b2945a8fa8
mike <57697589+desrev3r@users.noreply.github.com>
2022-03-13 13:19:10 +0400

    Delete telegram directory

--- telegram/src/@core/bot.ts
+++
@@ -1,8 +0,0 @@
-import { Telegraf } from "telegraf";
-import { TelegramContext } from "@types";
-
-export class TelegramBot extends Telegraf<TelegramContext> {
-  constructor(token: string) {
-    super(token);
-  }
-}
--- telegram/src/scenes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Stage } from "@core";
-import { Scenes } from "telegraf";
-
-import { start } from "./start.scene";
-import { wallet } from "./wallet.scene";
-import { settings } from "./settings.scene";
-
-export const stage = new Scenes.Stage([start, wallet, settings]);
